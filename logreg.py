03a928f5a59598276cf4daf3f25c97437d4a32c3
Álvaro García Carrasco <alvaro@naturalcycles.com>
2018-04-11 16:16:59 +0200

    add log reg

--- logreg.py
+++ logreg.py
@@ -1,154 +1,103 @@
+""" Solution for simple logistic regression model for MNIST
+with tf.data module
+MNIST dataset: yann.lecun.com/exdb/mnist/
+Created by Chip Huyen (chiphuyen@cs.stanford.edu)
+CS20: "TensorFlow for Deep Learning Research"
+cs20.stanford.edu
+Lecture 03
+"""
 import os
-import gzip
-import shutil
-import struct
-import urllib
+import tensorflow as tf
+import time
+import examples.utils as utils
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-from matplotlib import pyplot as plt
-import numpy as np
-import tensorflow as tf
+# Define paramaters for the model
+learning_rate = 0.01
+batch_size = 128
+n_epochs = 30
+n_train = 60000
+n_test = 10000
 
+# Step 1: Read in data
+mnist_folder = 'data/mnist'
+utils.download_mnist(mnist_folder)
 
-def huber_loss(labels, predictions, delta=14.0):
-    residual = tf.abs(labels - predictions)
+train, val, test = utils.read_mnist(mnist_folder, flatten=True)
 
-    def f1(): return 0.5 * tf.square(residual)
+# Step 2: Create datasets and iterator
+train_data = tf.data.Dataset.from_tensor_slices(train)
 
-    def f2(): return delta * residual - 0.5 * tf.square(delta)
+# train_data = train_data.shuffle(10000) # if you want to shuffle your data
+train_data = train_data.batch(batch_size)
 
-    return tf.cond(residual < delta, f1, f2)
+test_data = tf.data.Dataset.from_tensor_slices(test)
+test_data = test_data.batch(batch_size)
 
+iterator = tf.data.Iterator.from_structure(train_data.output_types,
+                                           train_data.output_shapes)
+img, label = iterator.get_next()
 
-def safe_mkdir(path):
-    """ Create a directory if there isn't one already. """
+train_init = iterator.make_initializer(train_data)  # initializer for train_data
+test_init = iterator.make_initializer(test_data)  # initializer for train_data
+
+# Step 3: create weights and bias
+# w is initialized to random variables with mean of 0, stddev of 0.01
+# b is initialized to 0
+# shape of w depends on the dimension of X and Y so that Y = tf.matmul(X, w)
+# shape of b depends on Y
+w = tf.get_variable(name='weights', shape=(784, 10), initializer=tf.random_normal_initializer(0, 0.01))
+b = tf.get_variable(name='bias', shape=(1, 10), initializer=tf.zeros_initializer())
+
+# Step 4: build model
+# the model that returns the logits.
+# this logits will be later passed through softmax layer
+logits = tf.matmul(img, w) + b
+
+# Step 5: define loss function
+# use cross entropy of softmax of logits as the loss function
+entropy = tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits, labels=label, name='entropy')
+loss = tf.reduce_mean(entropy, name='loss')  # computes the mean over all the examples in the batch
+
+# Step 6: define training op
+# using gradient descent with learning rate of 0.01 to minimize loss
+optimizer = tf.train.AdamOptimizer(learning_rate).minimize(loss)
+
+# Step 7: calculate accuracy with test set
+preds = tf.nn.softmax(logits)
+correct_preds = tf.equal(tf.argmax(preds, 1), tf.argmax(label, 1))
+accuracy = tf.reduce_sum(tf.cast(correct_preds, tf.float32))
+
+writer = tf.summary.FileWriter('./graphs/logreg', tf.get_default_graph())
+with tf.Session() as sess:
+    start_time = time.time()
+    sess.run(tf.global_variables_initializer())
+
+    # train the model n_epochs times
+    for i in range(n_epochs):
+        sess.run(train_init)  # drawing samples from train_data
+        total_loss = 0
+        n_batches = 0
+        try:
+            while True:
+                _, l = sess.run([optimizer, loss])
+                total_loss += l
+                n_batches += 1
+        except tf.errors.OutOfRangeError:
+            pass
+        print('Average loss epoch {0}: {1}'.format(i, total_loss / n_batches))
+    print('Total time: {0} seconds'.format(time.time() - start_time))
+
+    # test the model
+    sess.run(test_init)  # drawing samples from test_data
+    total_correct_preds = 0
     try:
-        os.mkdir(path)
-    except OSError:
+        while True:
+            accuracy_batch = sess.run(accuracy)
+            total_correct_preds += accuracy_batch
+    except tf.errors.OutOfRangeError:
         pass
 
-
-def read_birth_life_data(filename):
-    """
-    Read in birth_life_2010.txt and return:
-    data in the form of NumPy array
-    n_samples: number of samples
-    """
-    text = open(filename, 'r').readlines()[1:]
-    data = [line[:-1].split('\t') for line in text]
-    births = [float(line[1]) for line in data]
-    lifes = [float(line[2]) for line in data]
-    data = list(zip(births, lifes))
-    n_samples = len(data)
-    data = np.asarray(data, dtype=np.float32)
-    return data, n_samples
-
-
-def download_one_file(download_url,
-                      local_dest,
-                      expected_byte=None,
-                      unzip_and_remove=False):
-    """
-    Download the file from download_url into local_dest
-    if the file doesn't already exists.
-    If expected_byte is provided, check if
-    the downloaded file has the same number of bytes.
-    If unzip_and_remove is True, unzip the file and remove the zip file
-    """
-    if os.path.exists(local_dest) or os.path.exists(local_dest[:-3]):
-        print('%s already exists' % local_dest)
-    else:
-        print('Downloading %s' % download_url)
-        local_file, _ = urllib.request.urlretrieve(download_url, local_dest)
-        file_stat = os.stat(local_dest)
-        if expected_byte:
-            if file_stat.st_size == expected_byte:
-                print('Successfully downloaded %s' % local_dest)
-                if unzip_and_remove:
-                    with gzip.open(local_dest, 'rb') as f_in, open(local_dest[:-3], 'wb') as f_out:
-                        shutil.copyfileobj(f_in, f_out)
-                    os.remove(local_dest)
-            else:
-                print('The downloaded file has unexpected number of bytes')
-
-
-def download_mnist(path):
-    """
-    Download and unzip the dataset mnist if it's not already downloaded
-    Download from http://yann.lecun.com/exdb/mnist
-    """
-    safe_mkdir(path)
-    url = 'http://yann.lecun.com/exdb/mnist'
-    filenames = ['train-images-idx3-ubyte.gz',
-                 'train-labels-idx1-ubyte.gz',
-                 't10k-images-idx3-ubyte.gz',
-                 't10k-labels-idx1-ubyte.gz']
-    expected_bytes = [9912422, 28881, 1648877, 4542]
-
-    for filename, byte in zip(filenames, expected_bytes):
-        download_url = os.path.join(url, filename)
-        local_dest = os.path.join(path, filename)
-        download_one_file(download_url, local_dest, byte, True)
-
-
-def parse_data(path, dataset, flatten):
-    if dataset != 'train' and dataset != 't10k':
-        raise NameError('dataset must be train or t10k')
-
-    label_file = os.path.join(path, dataset + '-labels-idx1-ubyte')
-    with open(label_file, 'rb') as file:
-        _, num = struct.unpack(">II", file.read(8))
-        labels = np.fromfile(file, dtype=np.int8)  # int8
-        new_labels = np.zeros((num, 10))
-        new_labels[np.arange(num), labels] = 1
-
-    img_file = os.path.join(path, dataset + '-images-idx3-ubyte')
-    with open(img_file, 'rb') as file:
-        _, num, rows, cols = struct.unpack(">IIII", file.read(16))
-        imgs = np.fromfile(file, dtype=np.uint8).reshape(num, rows, cols)  # uint8
-        imgs = imgs.astype(np.float32) / 255.0
-        if flatten:
-            imgs = imgs.reshape([num, -1])
-
-    return imgs, new_labels
-
-
-def read_mnist(path, flatten=True, num_train=55000):
-    """
-    Read in the mnist dataset, given that the data is stored in path
-    Return two tuples of numpy arrays
-    ((train_imgs, train_labels), (test_imgs, test_labels))
-    """
-    imgs, labels = parse_data(path, 'train', flatten)
-    indices = np.random.permutation(labels.shape[0])
-    train_idx, val_idx = indices[:num_train], indices[num_train:]
-    train_img, train_labels = imgs[train_idx, :], labels[train_idx, :]
-    val_img, val_labels = imgs[val_idx, :], labels[val_idx, :]
-    test = parse_data(path, 't10k', flatten)
-    return (train_img, train_labels), (val_img, val_labels), test
-
-
-def get_mnist_dataset(batch_size):
-    # Step 1: Read in data
-    mnist_folder = 'data/mnist'
-    download_mnist(mnist_folder)
-    train, val, test = read_mnist(mnist_folder, flatten=False)
-
-    # Step 2: Create datasets and iterator
-    train_data = tf.data.Dataset.from_tensor_slices(train)
-    train_data = train_data.shuffle(10000)  # if you want to shuffle your data
-    train_data = train_data.batch(batch_size)
-
-    test_data = tf.data.Dataset.from_tensor_slices(test)
-    test_data = test_data.batch(batch_size)
-
-    return train_data, test_data
-
-
-def show(image):
-    """
-    Render a given numpy.uint8 2D array of pixel data.
-    """
-    plt.imshow(image, cmap='gray')
-    plt.show()
+    print('Accuracy {0}'.format(total_correct_preds / n_test))
+writer.close()
